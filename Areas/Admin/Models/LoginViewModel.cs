074927f4ae08e20971accf8bb9531aa47196c570
lenaick <lenaick.moreira@gmail.com>
2018-12-03 17:42:39 +0100

    Modifications BO

---
+++ Areas/Admin/Models/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projet3.Areas.Admin.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Courrier électronique")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Mot de passe")]
+        public string Password { get; set; }
+    }
+}
