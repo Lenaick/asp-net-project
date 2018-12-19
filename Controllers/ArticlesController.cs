7239a0787258f3ae969fd14fd052d4f319016c3b
lenaick <lenaick.moreira@gmail.com>
2018-12-19 21:16:19 +0100

    Sauvegarde

--- Controllers/ArticlesController.cs
+++ Controllers/ArticlesController.cs
@@ -55,5 +55,24 @@ namespace Projet3.Controllers
 
             return RedirectToAction("FicheArticle", new { id = id });
         }
+
+        // GET: Articles/Liste?idCategorie=1&search=php&page=1
+        public ActionResult Liste(int? idCategorie, string search, int? page)
+        {
+            int limit = 10;
+            int offset = 0;
+
+            if (null != page && page > 1)
+            {
+                offset = limit * (page.GetValueOrDefault() - 1);
+            }
+
+            int nbArticles = db.DerniersArticlesListe(idCategorie, search, null, null).ToList().Count;
+            decimal decNbPages = Convert.ToDecimal(nbArticles) / limit;
+            ViewBag.NbPages = Convert.ToInt32(Math.Ceiling(decNbPages));
+            ViewBag.Categories = db.Categorie.ToList();
+            var articles = db.DerniersArticlesListe(idCategorie, search, limit, offset).ToList();
+            return View(articles);
+        }
     }
 }
